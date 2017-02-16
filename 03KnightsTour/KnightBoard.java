c0e77d7296d93fc76c117f7ee7675fa4fde70881
Patrick Chan <patrickchan1999@gmail.com>
2017-02-16 15:33:17 -0500

    Still working on it, getting warmer

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -10,23 +10,50 @@ public class KnightBoard{
     }
 
     public boolean solveH(int row ,int col, int level){
-	if(col >= board.length || row >= board.length || col < 0 || row < 0){
-	    return false;
-	}
 	if(level == board.length * board[0].length){
 	    return true;
 	}
+	if(col >= board[0].length || row >= board.length || col < 0 || row < 0){
+	    return false;
+	}
 	if(board[row][col] == 0){
 	    board[row][col] = level;
-	    return solveH(row + 1, col + 2, level + 1) || 
+	    if( solveH(row + 1, col + 2, level + 1) || 
 		solveH(row + 1, col - 2, level + 1) ||
 		solveH(row + 2, col + 1, level + 1) ||
 		solveH(row + 2, col - 1, level + 1) ||
 		solveH(row - 2, col + 1, level + 1) ||
 		solveH(row - 2, col - 1, level + 1) ||
 		solveH(row - 1, col + 2, level + 1) ||
-		solveH(row - 1, col - 2, level + 1);
+		solveH(row - 1, col - 2, level + 1)){
+		return true;
+	    }
+	    else{
+		board[row][col] = 0;
+	    }
 	}
 	return false;
+    }
+
+    public String toString(){
+	String ans = "";
+	for (int r = 0; r < board.length; r++){
+	    for (int c = 0; c < board.length; c++){
+		if(board[r][c] >= 10){
+		    ans += board[r][c] + " ";
+		}
+		else{
+		    ans += " " + board[r][c] + " ";
+		}
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+
+    public static void main(String[] args){
+	KnightBoard k1 = new KnightBoard(5, 5);
+	k1.solve();
+	System.out.println(k1);
     }
 }
