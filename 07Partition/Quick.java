c2894a9909912475d6cc1aff187c8114f00f9aac
Jeremy Sharapov <jsharapov@stuy.edu>
2017-03-20 04:15:23 -0400

    Fixed part, but Quickselect seems to throw a negative exception every other run

--- 07Partition/Quick.java
+++ 07Partition/Quick.java
@@ -3,35 +3,44 @@ public class Quick{
 
 
     public static int part(int[] data, int start, int end){
-        int pivotIndex = new Random().nextInt(end - start + 1) + start;
+        int pivotIndex = new Random().nextInt(end - start) + start;
 	int pivot = data[pivotIndex];
-	data[pivotIndex] = data[start];
-	data[start] = pivot;
+	Swap(data, pivotIndex, end);
 	pivotIndex = start;
-	for(int i = start + 1; i < end; i++){
+	for(int i = start; i < end; i++){
 	    if(data[i] < pivot){
-		data[pivotIndex] = data[i];
-		data[i] = pivot;
-		pivotIndex = i;
+		Swap(data, i, pivotIndex);
+		pivotIndex++;
 	    }
 	}
+	Swap(data, end, pivotIndex);
 	return pivotIndex;
     }
 
-
+    public static void Swap(int[] data, int here, int there){
+	int temp = data[here];
+	data[here] = data[there];
+	data[there] = temp;
+    }
+    
     public static int quickselect(int[]data, int k){
 	int start = 0;
 	int end = data.length - 1;
 	int guess = part(data, start, end);
-	while (k != guess + 1){
-	    if (guess + 1 > k){
+	while (k != guess){
+	    if (guess > k){
 		end = guess - 1;
 	    }
-	    if (guess + 1 < k){
+	    if (guess < k){
 		start = guess + 1;
 	    }
 	    guess = part(data, start, end);
 	}
 	return data[guess];
+    }
+
+    public static void main(String[] args){
+	int[] bob = {8, 5, 0, 9, 3, 1, 4, 6, 2, 7};
+	System.out.println(quickselect(bob, 9));
     }
 }
