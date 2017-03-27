5fad6010ae535ea0916e8c12ffca1c3b1680fb86
Jeremy Sharapov <jsharapov@stuy.edu>
2017-03-27 01:31:27 -0400

    Finished, I think. Didn't get to test both this and mergesort on large arrays, but both work in the three specific cases you mentioned

--- 07Partition/Quick.java
+++ 07Partition/Quick.java
@@ -2,22 +2,35 @@ import java.util.*;
 public class Quick{
 
 
-    public static int part(int[] data, int start, int end){
-        int pivotIndex = new Random().nextInt(end - start) + start;
+    public static int[] part(int[] data, int start, int end){
+        int pivotIndex = new Random().nextInt(end - start + 1) + start;
 	int pivot = data[pivotIndex];
-	Swap(data, pivotIndex, end);
+	swap(data, pivotIndex, start);
 	pivotIndex = start;
-	for(int i = start; i < end; i++){
-	    if(data[i] < pivot){
-		Swap(data, i, pivotIndex);
-		pivotIndex++;
+	int lt = start;
+	int i = start + 1;
+	int gt = end;
+	while (i <= gt){
+	    if (data[i] == pivot){
+		i++;
+	    }
+	    else if (data[i] < pivot){
+		swap(data, i, lt);
+		lt++;
+		i++;
+	    }
+	    else{
+		swap(data, i, gt);
+		gt--;
 	    }
 	}
-	Swap(data, end, pivotIndex);
-	return pivotIndex;
+	int[] range = new int[2];
+	range[0] = lt;
+	range[1] = gt;
+	return range;
     }
 
-    public static void Swap(int[] data, int here, int there){
+    public static void swap(int[] data, int here, int there){
 	int temp = data[here];
 	data[here] = data[there];
 	data[there] = temp;
@@ -26,21 +39,28 @@ public class Quick{
     public static int quickselect(int[]data, int k){
 	int start = 0;
 	int end = data.length - 1;
-	int guess = part(data, start, end);
-	while (k != guess){
-	    if (guess > k){
-		end = guess - 1;
+	int[] guess = part(data, start, end);
+	while (!(guess[0] <= k && k <= guess[1])){
+	    if (guess[0] > k){
+		end = guess[0] - 1;
 	    }
-	    if (guess < k){
-		start = guess + 1;
+	    if (guess[1] < k){
+		start = guess[1] + 1;
 	    }
 	    guess = part(data, start, end);
 	}
-	return data[guess];
+	return data[guess[0]];
     }
 
-    public static void main(String[] args){
-	int[] bob = {8, 5, 0, 9, 3, 1, 4, 6, 2, 7};
-	System.out.println(quickselect(bob, 9));
+    public static void quicksort(int[] data){
+	quicksortH(data, 0, data.length - 1);
+    }
+
+    public static void quicksortH(int[] data, int start, int end){
+	if (start < end){
+	    int[] splitter = part(data, start, end);
+	    quicksortH(data, start, splitter[0]);
+	    quicksortH(data, splitter[1] + 1, end);
+	}
     }
 }
